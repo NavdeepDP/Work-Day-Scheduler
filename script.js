a26beef1ef52abbffb8d7a02770c252f1e455f7c
NP <navdeep.puri@gmail.com>
2020-09-11 20:32:53 -0400

    Modified code to store scheduled events in local storage

--- script.js
+++ script.js
@@ -17,39 +17,93 @@ $(document).ready(function () {
     var currentDayVar = moment(new Date());
     var currentDayString = currentDayVar.format("dddd, MMM do")
     console.log(currentDayVar.format("dddd, MMM do"));
-
     currentDayElement.text(currentDayString);
+
+    var startTime = 9;
 
     /**
      * FUNCTION DEFINITIONS
      * 
      */
-    for (var i = 1; i <= 9; i++) {
+    // get the current hour
+    const timeNow = moment();
+    var hourNow = timeNow.hour();
+    console.log("now: " + hourNow);
+    hourNow = 14;
 
-        
-        var newRow = $("<div>");        
+    function getEventScheduled(timeTag) {
+        var saveScheduledEvents = [];
+        var timeTagFound = false;
+        var local = JSON.parse(localStorage.getItem("calenderEvents"));
+        if (local !== null) {
+            saveScheduledEvents = local;
+
+            for (var i = 0; i < saveScheduledEvents.length; i++) {
+                if (saveScheduledEvents[i].time === parseInt(timeTag)) {
+                    return(saveScheduledEvents[i].event);
+                    timeTagFound = true;
+                    
+                }
+            }
+        }
+        if (!timeTagFound)
+            return "";
+    }
+
+    for (var i = 0; i < 9; i++) {
+
+        var currentRowTimeTag;
+        currentRowTimeTag = startTime + i;
+
+
+
+        var newRow = $("<div>");
         newRow.addClass("row");
 
+        //Working hours time div
         var newHourDiv = $("<div>");
         newHourDiv.addClass("col-sm-1 hour");
-        newHourDiv.text("9AM");      
+        if (startTime + i > 12) {
+            newHourDiv.text(((startTime + i) - 12) + "PM");
+        }
+        else if ((startTime + i) === 12) {
+            newHourDiv.text("12PM");
+        }
+        else {
+            newHourDiv.text((startTime + i) + "AM");
+        }
         newRow.append(newHourDiv);
+
+        // check to decide past , present , future class
+        var timeClassName = "";
+        if (hourNow > currentRowTimeTag)
+            timeClassName = "past";
+        else if (hourNow < currentRowTimeTag)
+            timeClassName = "future";
+        else
+            timeClassName = "present";
+
+
+        var eventText = getEventScheduled(currentRowTimeTag);
 
         var newTextDiv = $("<div>")
         newTextDiv.addClass("col-sm-10 divPadding");
         var textAreaVar = $("<textarea>");
-        textAreaVar.addClass("past");
+        textAreaVar.addClass(timeClassName);
+        textAreaVar.attr("id", "event-" + currentRowTimeTag);
+        textAreaVar.text(eventText);
         newTextDiv.append(textAreaVar);
         newRow.append(newTextDiv);
 
         var newBtnDiv = $("<div>")
         newBtnDiv.addClass("col-sm-1 divPadding");
         var btnVar = $("<button>");
-        btnVar.addClass("btn saveBtn");        
+        btnVar.addClass("btn saveBtn");
         btnVar.addClass("fa fa-save");
+        btnVar.attr("data-index", currentRowTimeTag);
         newBtnDiv.append(btnVar);
         newRow.append(newBtnDiv);
-       
+
         divContainer.append(newRow);
     }
 
@@ -61,6 +115,36 @@ $(document).ready(function () {
     /**
      * EVENT HANDLERS
      */
+
+    $(document).on("click", ".saveBtn", function (event) {
+
+        console.log("save button clicked:");
+        console.log($(this).attr("data-index"));
+
+        var timeTag = $(this).attr("data-index");
+        var id = "#" + "event-" + timeTag;
+        var eventScheduled = $(id).val();
+
+        var saveScheduledEvents = [];
+        var timeTagFound = false;
+        var local = JSON.parse(localStorage.getItem("calenderEvents"));
+        if (local !== null) {
+            saveScheduledEvents = local;
+
+            for (var i = 0; i < saveScheduledEvents.length; i++) {
+                if (saveScheduledEvents[i].time === parseInt(timeTag)) {
+                    saveScheduledEvents[i].event = eventScheduled;
+                    timeTagFound = true;
+                    break;
+                }
+            }
+        }
+        if (!timeTagFound)
+            saveScheduledEvents.push({ time: parseInt(timeTag), event: eventScheduled });
+
+        localStorage.setItem("calenderEvents", JSON.stringify(saveScheduledEvents));
+
+    });
 
 
 });
